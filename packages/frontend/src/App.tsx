5bcbf07b8ff5634e0e2d3cc0b585f766577df57f
Alvaro Basallo <alvaro@basallo.es>
2023-01-20 15:33:20 +0000

    React cannot access GraphQL directly.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,9 +3,6 @@ import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import { ApolloProvider } from 'react-apollo'
-import { apolloClient } from './services/graphql/apolloClient'
-
 import { MuiThemeProvider, StylesProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
@@ -16,18 +13,16 @@ import Home from './views/home/Home'
 import constants from './utils/constants'
 
 const App = () => (
-  <ApolloProvider client={apolloClient}>
-    <MuiThemeProvider theme={customMUITheme}>
-      <StylesProvider injectFirst>
-        <CssBaseline />
-        <Router>
-          <Routes>
-            <Route path={constants.PATH_ROOT} element={<Home />} />
-          </Routes>
-        </Router>
-      </StylesProvider>
-    </MuiThemeProvider>
-  </ApolloProvider>
+  <MuiThemeProvider theme={customMUITheme}>
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path={constants.PATH_ROOT} element={<Home />} />
+        </Routes>
+      </Router>
+    </StylesProvider>
+  </MuiThemeProvider>
 )
 
 export default App
